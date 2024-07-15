b873ef5524ff95a00b3489be38bf16b48f21ebb0
ulnvkrll <ulnvkrll@gmail.com>
2024-07-15 18:46:20 +0300

    annotation

--- src/main/java/ru/maxima/controllers/HelloController.java
+++ src/main/java/ru/maxima/controllers/HelloController.java
@@ -3,6 +3,7 @@ package ru.maxima.controllers;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,7 +12,7 @@ public class HelloController {
 
     @GetMapping("/new-hello-world")
     public String newSayHello(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = true) String surname) {
+                              @RequestParam(value = "surname", required = false) String surname) {
         System.out.println("You are inside your controller");
         System.out.println(name.length() + " : " + surname);
         return "hello";
@@ -19,11 +20,32 @@ public class HelloController {
 
 
     @GetMapping("/old-hello-world")
-    public String oldSayHello(HttpServletRequest request) {
+    public String oldSayHello(HttpServletRequest request,
+                              Model model) {
         String name = request.getParameter("name");
         String surname = request.getParameter("surname");
+        Integer age = Integer.valueOf(request.getParameter("age"));
+
+        model.addAttribute("message", "Hello," + name + ", " + surname);
+        model.addAttribute("age", "He is " + age + " years old");
+
+        System.out.println(name + " : " + surname);
         System.out.println("YOu are inside your controller");
-        System.out.println(name.length() + " : " + surname);
         return "hello";
+    }
+
+
+
+
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello";
+    }
+
+
+    @GetMapping("/point")
+    public String privetupir() {
+        return "privet upir";
     }
 }
