49fa8e4229b7c8790bf6a5fbdd5d5f5ae1a9c420
ulnvkrll <ulnvkrll@gmail.com>
2024-08-03 12:17:30 +0300

    annotation

---
+++ src/main/java/ru/maxima/dao/PersonDAO.java
@@ -0,0 +1,127 @@
+package ru.maxima.dao;
+
+import org.springframework.stereotype.Component;
+import ru.maxima.model.Person;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Component
+public class PersonDAO {
+// Data access object
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static final Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public List<Person> getAllPeople() {
+        List<Person> allPeople = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "select * from person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getLong("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setMail(resultSet.getString("email"));
+                allPeople.add(person);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return allPeople;
+    }
+
+    public Person findById(Long id) {
+        Person person = null;
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "select * from person where id = " + id;
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                person = new Person();
+                person.setId(resultSet.getLong("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setMail(resultSet.getString("email"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return person;
+    }
+
+    public void save(Person person) {
+        Person maxByAge = getAllPeople()
+                .stream()
+                .max(Comparator.comparing(Person::getAge))
+                .orElseThrow(NoSuchElementException::new);
+        Long nextId = (maxByAge.getId() + 1);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "insert into person(id, name, age, email) values(" +
+                    nextId + ", '" +
+                    person.getName() + "' ," +
+                    person.getAge() + ", '" +
+                    person.getMail() + "'" +
+                    ")";
+            statement.executeUpdate(SQL);
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+        }
+    }
+
+    public void update(Long id, Person editedPerson) {
+        Long nextId = (long) (getAllPeople().size() +1);
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "update person " +
+                    "set name = '"+ editedPerson.getName() + "'" +
+                    ", age = " + editedPerson.getAge() +
+                    ", email = '" + editedPerson.getMail() + "'" +
+                    " where id = " +id;
+            statement.executeUpdate(SQL);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public void deleteById(Long id) {
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "delete from person where id = " +id;
+            statement.executeUpdate(SQL);
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
---
+++ src/main/java/ru/maxima/model/Person.java
@@ -0,0 +1,29 @@
+package ru.maxima.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Person {
+    private Long id;
+
+    @NotEmpty(message = "Name should not to be empty")
+    @Size(min = 2, max=30, message = "Name should be bertween 2 and 30 characters")
+    private String name;
+
+    @Min(value = 0, message = "Age should be more than 0")
+    private Integer age;
+
+    @NotEmpty(message = "Email should not to be empty")
+    @Email(message = "Email is not valid")
+    private String mail;
+
+
+}
